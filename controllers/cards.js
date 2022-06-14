24b80d399700ad1b8cc97ad4e742f64f454162c1
Noa <noashohat1996@gmail.com>
2022-06-15 00:43:18 +0300

    edit card

--- controllers/cards.js
+++ controllers/cards.js
@@ -43,15 +43,90 @@ module.exports = {
   getCustomerCardsDetails: async function (req, res, next) {
     //collection.find({customerId:value})
   },
-  editCardDetails: async function (req, res, next) {},
-  deleteCard: async function (req, res) {},
+  editCardDetails: async function (req, res, next) {
+    const reqBody = req.body;
 
-  deleteProduct: async function (req, res, next) {
-    // get client id that we want to delete
-    // validate: number not null
-    // const sql = DELETE
-    // return details of deleted product
+    const schema = joi.object({
+      businessName: joi.string().required().min(2).max(200),
+      businessDescription: Joi.string().min(2).max(1024).required(),
+      businessAddress: Joi.string().min(2).max(400).required(),
+      businessPhone: joi
+        .string()
+        .required()
+        .regex(/^[0-9]{8,11}$/),
+      businessPic: joi.string().min(11).max(1024),
+      customerId: Joi.objectId().required(),
+    });
 
+    const { error, value } = schema.validate(reqBody);
+
+    if (error) {
+      res.status(400).send(`error update product: ${error}`);
+      return;
+    }
+
+    const keys = Object.keys(value); // ['name','price']
+    const values = Object.values(value); // ['aaa', 15]
+    const fields = keys.map((key) => `${key}=?`).join(",");
+    values.push(req.params.id);
+
+    try {
+      const database = await mongo.getDb();
+      const collection = database.collection("cards");
+
+      collection.update({ id: value });
+      res.json({
+        id: value.id,
+      });
+    } catch (err) {
+      res.status(400).send("error updating card");
+      console.log(err.message);
+    }
+  },
+
+  //   editProduct: async function (req, res, next) {
+  //     const reqBody = req.body;
+
+  //     const schema = joi.object({
+  //         name: joi.string().min(2).max(100),
+  //         description: joi.string().min(2).max(300),
+  //         price: joi.number(),
+  //         image: joi.string().min(5).max(200),
+  //     }).min(1);
+
+  //     const { error, value } = schema.validate(reqBody);
+
+  //     if (error) {
+  //         res.status(400).send(`error update product: ${error}`);
+  //         return;
+  //     }
+  //     /*
+  //     {
+  //         name: 'aaa',
+  //         price: 15,
+  //     }
+  //     */
+
+  //     const keys = Object.keys(value);   // ['name','price']
+  //     const values = Object.values(value); // ['aaa', 15]
+  //     // const fields = keys.map(key => `${key}=?`); // ['name=?','price=?']
+  //     // const parseFileds = fields.join(','); // 'name=?,price=?'
+  //     const fields = keys.map(key => `${key}=?`).join(',');
+  //     values.push(req.params.id);
+  //     const sql = `UPDATE products SET ${fields} WHERE id=?`;
+
+  //     try {
+  //         const result = await database.query(sql, values);
+  //         res.json(value);
+  //     }
+  //     catch (err) {
+  //         console.log(err);
+  //         return;
+  //     }
+
+  // },
+
+  deleteCard: async function (req, res) {
     const schema = joi.object({
       id: joi.number().required(),
     });
